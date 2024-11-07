9f449b2fbb11abbafc01f0195927bf79361f2eb2
angel060406 <angelpollo060@gmail.com>
2024-11-06 22:34:01 -0600

    [ADD] router & routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-// import Datas from './pages/datas/Datas'
-// import Login from './pages/login/Login'
-// import Welcome from './pages/welcome/Welcome'
-// import Register from './pages/register/Register'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//import { UserProvider } from './Components/context/UserContext'; Para proteger home
+import Datas from './pages/datas/Datas'
+import Login from './pages/login/Login'
+import Welcome from './pages/welcome/Welcome'
+import Register from './pages/register/Register'
 import Home from './pages/home/Home'
-// import Profile from './pages/profile/Profile'
-// import Estadistics from './pages/estadists/Estadistics'`
+import Profile from './pages/profile/Profile'
+import Estadistics from './pages/estadists/Estadistics'
 
 function App() {
 
 
   return (
-    <>
-      <Home></Home>
-    </>
+    <BrowserRouter>
+      <Routes>
+      <Route path="" element={<Welcome />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="home" element={<Home />} />
+        <Route path="datas" element={<Datas />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="estadistics" element={<Estadistics />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
