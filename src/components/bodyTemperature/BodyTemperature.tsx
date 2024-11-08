fefc556064ce6ddbdc8434a990e67d3678cfc7cb
angel060406 <angelpollo060@gmail.com>
2024-11-07 18:46:14 -0600

    [UPDATE] ws connection to body temperature

--- src/components/bodyTemperature/BodyTemperature.tsx
+++ src/components/bodyTemperature/BodyTemperature.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface BodyTemperatureProps {
-  temperature: number | string; // La temperatura puede ser un número o '--' como valor inicial
+  initialTemperature?: number | string;
 }
 
-const BodyTemperature: React.FC<BodyTemperatureProps> = ({ temperature }) => {
+const BodyTemperature: React.FC<BodyTemperatureProps> = ({ initialTemperature = '--' }) => {
+  const [temperature, setTemperature] = useState<number | string>(initialTemperature);
+
+  useEffect(() => {
+    // Conexión WebSocket
+    const ws = new WebSocket('ws://localhost:3000');
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.temperature) {
+        setTemperature(data.temperature);
+      }
+    };
+
+    return () => ws.close();
+  }, []);
+
   return (
     <div className="bg-gray-900 p-4 rounded-lg text-white w-full text-center">
       <h2 className="text-sm font-bold">TEMPERATURA CORPORAL</h2>
-      <p className="text-xl mt-2">{temperature}54 °C</p>
+      <p className="text-xl mt-2">{temperature} °C</p>
     </div>
   );
 };
