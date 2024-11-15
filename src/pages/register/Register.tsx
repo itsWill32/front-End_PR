770f7e8a9d5da2da69970ccdf38d1c646132f146
060406 <angelpollo060@gmail.com>
2024-11-15 04:10:43 -0600

    [UPDATE] register has been connected to API

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,13 +1,50 @@
-import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Register.css';
 import logoAthleteBand from "../../assets/ATHLETEBANDLogo.png";
 
 export default function Register() {
-  const [showPassword, setShowPassword] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setErrorMessage("Las contraseñas no coinciden");
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSuccessMessage("Registro exitoso. Redirigiendo...");
+        setTimeout(() => navigate('/login'), 2000); // Redirige al login después de 2 segundos
+      } else {
+        setErrorMessage(data.message || 'Error al registrarse');
+      }
+    } catch (error) {
+      console.error('Error al registrar:', error);
+      setErrorMessage('Ocurrió un error al conectar con el servidor');
+    }
+  };
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-[#000000] via-[#1E3545] to-[#000000] bg-[length:200%] animate-gradient-x flex items-center justify-center">
@@ -21,7 +58,14 @@ export default function Register() {
         </div>
         <h2 className="text-center text-xl font-bold mb-6">ATHLETIC-BAND</h2>
 
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleRegister}>
+          {errorMessage && (
+            <p className="text-red-500 text-center">{errorMessage}</p>
+          )}
+          {successMessage && (
+            <p className="text-green-500 text-center">{successMessage}</p>
+          )}
+
           <div className="space-y-4">
             {/* Nombre */}
             <div className="relative">
@@ -32,8 +76,11 @@ export default function Register() {
                 type="text"
                 id="name"
                 name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 placeholder="Escribe tu nombre"
                 className="w-full px-3 py-2 bg-transparent border-b-2 border-gray-500 focus:outline-none placeholder-gray-300 text-sm transition-all duration-300 ease-in-out"
+                required
               />
             </div>
 
@@ -46,8 +93,11 @@ export default function Register() {
                 type="email"
                 id="email"
                 name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Escribe tu correo"
                 className="w-full px-3 py-2 bg-transparent border-b-2 border-gray-500 focus:outline-none placeholder-gray-300 text-sm transition-all duration-300 ease-in-out"
+                required
               />
             </div>
 
@@ -60,25 +110,18 @@ export default function Register() {
                 type={showPassword ? "text" : "password"}
                 id="password"
                 name="password"
-                placeholder="••••••••"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                placeholder="••••••••"
                 className="w-full px-3 py-2 bg-transparent border-b-2 border-gray-500 focus:outline-none placeholder-gray-300 text-sm transition-all duration-300 ease-in-out"
+                required
               />
               <button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
                 className="absolute right-2 top-9 text-gray-500 hover:text-gray-300 transition"
               >
-                {showPassword ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-5.523 0-10-3.85-10-7.5S6.477 4 12 4c1.35 0 2.633.249 3.825.675M15 15l5 5m0 0l-5-5m5 5H9" />
-                  </svg>
-                ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 15l5 5m0 0l-5-5m5 5H9m4-2a2 2 0 100-4 2 2 0 000 4z" />
-                  </svg>
-                )}
+                {/* Icono */}
               </button>
             </div>
 
@@ -91,25 +134,18 @@ export default function Register() {
                 type={showConfirmPassword ? "text" : "password"}
                 id="confirm-password"
                 name="confirm-password"
-                placeholder="••••••••"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="••••••••"
                 className="w-full px-3 py-2 bg-transparent border-b-2 border-gray-500 focus:outline-none placeholder-gray-300 text-sm transition-all duration-300 ease-in-out"
+                required
               />
               <button
                 type="button"
                 onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                 className="absolute right-2 top-9 text-gray-500 hover:text-gray-300 transition"
               >
-                {showConfirmPassword ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-5.523 0-10-3.85-10-7.5S6.477 4 12 4c1.35 0 2.633.249 3.825.675M15 15l5 5m0 0l-5-5m5 5H9" />
-                  </svg>
-                ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 15l5 5m0 0l-5-5m5 5H9m4-2a2 2 0 100-4 2 2 0 000 4z" />
-                  </svg>
-                )}
+                {/* Icono */}
               </button>
             </div>
           </div>
@@ -120,19 +156,17 @@ export default function Register() {
               type="submit"
               className="w-full px-4 py-2 border border-white rounded-md hover:bg-white hover:text-black transition duration-200 text-sm font-semibold"
             >
-              <Link to={"/wel"}>
-                REGISTRARSE
-              </Link>
+              REGISTRARSE
             </button>
           </div>
 
           {/* Link para Iniciar Sesión */}
           <div className="text-center mt-5">
             <p className="text-sm border-b-2 border-transparent hover:border-gray-300 inline-block pb-1 transition duration-300 ease-in-out">
-              YA TIENES UNA CUENTA? 
-              <Link to={"/login"}>
+              YA TIENES UNA CUENTA?{' '}
+              <a href="/login" className="text-white">
                 INICIA SESIÓN
-              </Link>
+              </a>
             </p>
           </div>
         </form>
